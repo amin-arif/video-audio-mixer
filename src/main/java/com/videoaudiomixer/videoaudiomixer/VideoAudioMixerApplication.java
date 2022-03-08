7931487eb99bbb1f0a719f8bb7f5ad3b85d691cb
arif <mdarifkanon@gmail.com>
2022-03-08 07:19:51 +0600

    initial commit

---
+++ src/main/java/com/videoaudiomixer/videoaudiomixer/VideoAudioMixerApplication.java
@@ -0,0 +1,13 @@
+package com.videoaudiomixer.videoaudiomixer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoAudioMixerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoAudioMixerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/videoaudiomixer/videoaudiomixer/VideoAudioMixerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.videoaudiomixer.videoaudiomixer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoAudioMixerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
