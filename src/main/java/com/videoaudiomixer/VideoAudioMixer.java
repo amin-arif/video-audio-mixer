06debfe3422cbe35c68afba356ca0bb155990106
arif <mdarifkanon@gmail.com>
2022-03-09 06:25:10 +0600

    Remove unnecessary files | Added a main file to control ffmpeg

---
+++ src/main/java/com/videoaudiomixer/VideoAudioMixer.java
@@ -0,0 +1,15 @@
+package com.videoaudiomixer;
+
+public class VideoAudioMixer {
+    private String ffmpegExe;
+
+    public VideoAudioMixer(String ffmpegExe) {
+        super();
+        this.ffmpegExe = ffmpegExe;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello Arif");
+    }
+}
